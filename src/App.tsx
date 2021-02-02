6c59fd5d33695b100537bab0332aa6618c7b8d06
nailseong <nnis9857@gmail.com>
2021-02-03 05:23:45 +0900

    Delete dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import { useReactiveVar } from "@apollo/client";
 import { isLoggedInVar } from "./apollo";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { darkTheme, lightTheme } from "./config";
 import { LoggedInRouter } from "./router/logged-in-router";
 import { LoggedOutRouter } from "./router/logged-out-router";
+import { theme } from "./config";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  const [light, setLight] = React.useState(false);
 
   return (
-    <MuiThemeProvider theme={light ? lightTheme : darkTheme}>
+    <MuiThemeProvider theme={theme}>
       <CssBaseline />
       {isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />}
     </MuiThemeProvider>
--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export const darkTheme = createMuiTheme({
+export const theme = createMuiTheme({
   palette: {
     type: "dark",
     secondary: {
@@ -8,38 +8,6 @@ export const darkTheme = createMuiTheme({
       main: "#1b5e20",
       dark: "#003300",
       contrastText: "#ffffff",
-    },
-  },
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        body: {
-          fontSize: "14px",
-        },
-      },
-    },
-  },
-});
-
-export const lightTheme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#ffffff",
-      main: "#fafafa",
-      dark: "#c7c7c7",
-      contrastText: "#000000",
-    },
-    secondary: {
-      light: "#4c8c4a",
-      main: "#1b5e20",
-      dark: "#003300",
-      contrastText: "#ffffff",
-    },
-    background: {
-      default: "#fafafa",
-    },
-    text: {
-      primary: "#000000",
     },
   },
   overrides: {
