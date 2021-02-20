7314e78247d0d4ed7eb890ce0a3cc0570dc7f7a0
nailseong <nnis9857@gmail.com>
2021-02-21 08:07:33 +0900

    Improve Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,6 +6,10 @@ import Typography from "@material-ui/core/Typography";
 import { useHistory } from "react-router-dom";
 import { isLoggedInVar } from "../apollo";
 import Cookies from "js-cookie";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { useMe } from "../hooks/use-me";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 type IProps = {
   title: string;
@@ -26,11 +30,23 @@ const useStyles = makeStyles((theme) => ({
 export const Header = ({ title }: IProps) => {
   const history = useHistory();
   const classes = useStyles();
+  const { data, loading } = useMe();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <React.Fragment>
       <Toolbar className={classes.toolbar}>
         <Button
+          style={{ marginRight: "36px" }}
           size="small"
           onClick={() => {
             history.push("/");
@@ -54,16 +70,49 @@ export const Header = ({ title }: IProps) => {
           {title}
         </Typography>
         <Button
+          style={{ width: "100px" }}
           variant="outlined"
           size="small"
-          onClick={() => {
-            Cookies.remove("connect.sid");
-            isLoggedInVar(false);
-            history.push("/");
-          }}
+          onClick={handleClick}
         >
-          Logout
+          Account
         </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              if (loading) {
+                handleClose();
+              }
+              if (!loading && data) {
+                history.push(`/user/${data.me.username}`);
+              }
+            }}
+          >
+            Profile
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              history.push("/edit-account");
+            }}
+          >
+            Edit account
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              Cookies.remove("connect.sid");
+              isLoggedInVar(false);
+              history.push("/");
+            }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </React.Fragment>
   );
